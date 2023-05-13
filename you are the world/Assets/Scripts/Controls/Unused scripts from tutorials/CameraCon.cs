f844f430d00af92393a974d50513955beb8a54d9
elcaptainchef <elcaptainchef@gmail.com>
2023-05-13 10:46:56 -0700

    Pivot in Style
    all things followed failed to make a first person camera control system. i have no fuckin clue why all i know is that i have no idea what i am doing. lili suggest point and click and i dont fuckin care anymore. ill try it next time.

---
+++ you are the world/Assets/Scripts/Controls/Unused scripts from tutorials/CameraCon.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCon : MonoBehaviour
+{
+
+    /*
+     * followed this from a tutorial
+     * i dont think it will be used but the Lerp thing adds a interesting trail
+     * not rigid glued to the character but smoother follow so i wanna keep the code to look at later.
+     * 
+    public Transform oreintation;
+    public float pLerp = .02f;
+    public float rLerp = .01f;
+
+    private void Update() {
+        transform.position = Vector3.Lerp(transform.position, oreintation.position, pLerp);
+        transform.rotation = Quaternion.Lerp(transform.rotation,oreintation.rotation, rLerp);
+    }
+    */
+}
